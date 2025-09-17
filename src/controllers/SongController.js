0bbf0ee8f3c9989e1e4ad82445abb2372bfbe9d3
Wilber Hinestroza <contactowillyber23@gmail.com>
2025-09-16 22:19:18 -0500

    Agrego nuevos archivos y cambios al proyecto

--- src/controllers/SongController.js
+++ src/controllers/SongController.js
@@ -13,15 +13,15 @@ export default class SongController {
       new Song(7, "Canción 6", "Berry", `${PUBLIC}/music/7.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
       new Song(8, "Canción 7", "Berry", `${PUBLIC}/music/8.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
       new Song(9, "Canción 8", "Berry", `${PUBLIC}/music/9.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
-      new Song(10, "Canción 9", "Berry", `${PUBLIC}/music/10.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
-      new Song(11, "Canción 10", "Berry", `${PUBLIC}/music/11.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
+      new Song(10, "Canción 9", "Berry", `${PUBLIC}/music/10NN.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
+      new Song(11, "Canción 10", "Berry", `${PUBLIC}/music/11NN.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
       new Song(12, "Comercial #2", "Berry", `${PUBLIC}/music/12.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
-      new Song(13, "Canción 11", "Berry", `${PUBLIC}/music/13.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
+      new Song(13, "Canción 11", "Berry", `${PUBLIC}/music/13NN.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
       new Song(14, "Canción 12", "Berry", `${PUBLIC}/music/14.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
       new Song(15, "Canción 13", "Berry", `${PUBLIC}/music/15.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
-      new Song(16, "Canción 14", "Berry", `${PUBLIC}/music/16.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
-      new Song(17, "Canción 15", "Berry", `${PUBLIC}/music/17.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
-      new Song(18, "Outro", "Berry", `${PUBLIC}/music/18.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
+      new Song(16, "Canción 14", "Berry", `${PUBLIC}/music/16NN.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
+      new Song(17, "Canción 15", "Berry", `${PUBLIC}/music/17NN.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
+      new Song(18, "Outro", "Berry", `${PUBLIC}/music/18NN.mp3`, `${PUBLIC}/cover/ktv23-cover.png`, `${PUBLIC}/cover/video.mp4`),
     ];
   }
 }
